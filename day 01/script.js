6e13d947060fd1cd7873264120674b77c965660f
clone <diminzali@gmail.com>
2024-06-11 18:36:50 +0100

    version du tutoriel de challenge 1

--- day 01/script.js
+++ day 01/script.js
@@ -1,17 +1,13 @@
-let photo = document.querySelectorAll('.photo')
-let description = document.querySelectorAll('.active')
-let k
-console.log("le nombre de photo est :", photo.length)
-for (let i = 0; i < photo.length; ++i) {
-     photo[i].addEventListener('click',()=>{
-             if (i != k) { 
-                for (let i = 0; i < photo.length; ++i){
-                        photo[i].style.width = '05em'
-                        description[i].style.display = 'none' 
-                }
-             }
-        photo[i].style.width = '30em'
-        k = i 
-        description[i].style.display = 'block'
-     })  
+const panels = document.querySelectorAll('.panel')
+panels.forEach(panel=>{
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses () {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
 }
