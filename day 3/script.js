36d9e25946c859bced42b13f8514dfefc37aa708
clone <diminzali@gmail.com>
2024-06-20 17:20:23 +0100

    la version tutoriel du projet 03

--- day 3/script.js
+++ day 3/script.js
@@ -1,25 +1,10 @@
-let btn_menu = document.getElementById('burger_menu')
-let menu_navigation = document.querySelector('nav')
-let btn_arrow = document.getElementById('arrow_menu')
-let btn_div_menu = document.querySelector('.div_menu')
-let papierPain_div_main = document.querySelector('.div_main')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
 
+open.addEventListener('click', ()=> container.classList.add('show-nav'))
 
-
-btn_menu.addEventListener('click', () =>{
-    btn_div_menu.classList.add('transform')
-    menu_navigation.classList.add ('active')
-    papierPain_div_main.classList.add('transform1')
-
-})
-
-btn_arrow.addEventListener('click', () =>{
-    btn_div_menu.classList.remove('transform')
-    menu_navigation.classList.remove ('active')
-    papierPain_div_main.classList.remove('transform1')
-
-
-})
+close.addEventListener('click', ()=> container.classList.remove('show-nav'))
 
 
