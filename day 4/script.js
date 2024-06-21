b15fae52ec3d22d64c5ba1c440b9a12c5a956d1a
clone <diminzali@gmail.com>
2024-06-21 12:07:08 +0100

    la version du tutoriel du projet04

--- day 4/script.js
+++ day 4/script.js
@@ -1,20 +1,9 @@
-let input = document.getElementById('input')
-let search_btn = document.getElementById('search_btn')
-let div_search = document.querySelector('.div_search')
-let clicked = true
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
 
-
-search_btn.addEventListener('click', (event)=> {
-    
-    if (clicked) {
-        div_search.classList.add('search_translate')
-        input.style.width = '150px'
-        clicked = false 
-    }else{
-        input.style.width = '0px'
-         div_search.classList.remove('search_translate')
-         clicked = true 
-        input.style.width = '0'
-    }
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
 })
