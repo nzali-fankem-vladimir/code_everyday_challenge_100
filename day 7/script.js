b87741b29c392bb192904161d5f1bef76e347b51
clone <diminzali@gmail.com>
2024-06-27 18:30:30 +0100

    la version tutorielle du projet 7

--- day 7/script.js
+++ day 7/script.js
@@ -1,22 +1,10 @@
-const side_ps_5 = document.querySelector('.div_ps5')
-const side_xbox = document.querySelector('.div_xbox')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
 
-
-side_ps_5.addEventListener('click', ()=> {
-    side_ps_5.style.width = '80%'
-    side_ps_5.style.transition = '.6s'
-    side_xbox.style.width = '20%'
-    side_xbox.style.transition = '.6s'
-})
-
-side_xbox.addEventListener('click', ()=> {
-    side_ps_5.style.width = '20%'
-    side_ps_5.style.transition = '.6s'
-    side_xbox.style.width = '80%'
-    side_xbox.style.transition = '.6s'
-})
-
-
-
-
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
