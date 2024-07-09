0b43fb2411d337a0021a26f40d4cdec58b5107e1
clone <diminzali@gmail.com>
2024-07-09 15:50:08 +0100

    version tutorielle du projet 09

--- day 9/script.js
+++ day 9/script.js
@@ -1,16 +1,25 @@
-let audios = document.querySelectorAll('.audio')
-let box_sounds = document.querySelectorAll('.box_sound')
-let previous_sound = 0
+const sounds =  ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
-for (let i = 0; i < box_sounds.length; i++) {
-    box_sounds[i].addEventListener('click', ()=> {
-        // audios.length+1 car le dernier box_soundsest à l'indice 5 qui ne respecte plus la condition de la boucle 
-      for (let k = 0; k < audios.length+1; k++) {
-        audios[i].play()
-        // on recupere l'indice du dernier son lu
-        previous_sound = k
-        audios[previous_sound].pause()
-        audios[previous_sound].currentTime = 0
-      }
-    })   
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0; 
+    })
 }
