d09dc05e6616388b8f5ae10d1730728c92965f9b
nzali-fankem-vladimir <diminzali@gmail.com>
2024-08-21 13:01:24 +0100

    version tutorielle du day13

--- day13/script.js
+++ day13/script.js
@@ -1,83 +1,64 @@
-let zone_text = document.querySelector('#zone_text')
-let div_choices = document.querySelector('.choices')
-let final_value = ''
-let new_choice = ''
-let count = 0
-let finish = false
-zone_text.focus()
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
+textarea.focus()
 
-new_choice = document.createElement('span')
-zone_text.addEventListener('keypress', (event)=>{
-    if (event.key !== 'Enter') {
-        if (event.key === ',') {
-            createElement ()
-            count++
-        }else {
-            div_choices.appendChild(new_choice)
-            new_choice.innerText += event.key
-        }
-    }else{
-
-        let min = 0
-        let max = count + 1
-        let timer_min = 0 
-        let timer_max = 10
-        random_choice_blue ()
-
-        if (finish === false) {
-            
-            let intervalId = setInterval(() => {
-                let randomNumber = Math.floor(Math.random() *(max-min)+min);
-                let nthChild = div_choices.children[(randomNumber)]
-                nthChild.style.backgroundColor = 'blue';
-                let intervalId1 = setInterval(() => {
-                    let previous_nthChild = nthChild
-                    previous_nthChild.style.backgroundColor = 'orange';
-                    if (timer_min >= timer_max) {
-                        clearInterval(intervalId)
-                        clearInterval(intervalId1)
-                        finish = true
-                    }
-                },600);
-                timer_min ++
-                
-            }, 350);
-            if (timer_min >= timer_max) {
-                clearInterval(intervalId)
-                finish = true
-            }
-        }else{
-            finish = false
-            console.log(count)
-            console.log('bien')
-        }
+textarea.addEventListener('keyup', (e)=>{
+    createTags(e.target.value)
+    if (e.key === 'Enter') {
+        setTimeout(()=>{
+            e.target.value =''
+        }, 10)
         
+        randomSelect()
     }
-        
 })
 
-function random_choice_blue () {
-    for (let i = 0; i < div_choices.children.length; i++) {
-        div_choices.children[i].style.backgroundColor = 'orange'; 
-    }
-    let min1 = 0
-    let max1 = count + 1
-    let random = Math.floor(Math.random() *(max1-min1)+min1);
-    let nthChild_output = div_choices.children[(random)]
-    nthChild_output.style.backgroundColor = 'blue';
+function createTags(input){
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML =''
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerHTML = tag
+        tagsEl.appendChild(tagEl)
+    });
 }
 
+function randomSelect (){
+    const times = 30
 
+    const interval = setInterval(() =>{
+        const randomTag = pickRandomTag()
 
-function createElement () {
-    div_choices.appendChild(new_choice)
-    new_choice = document.createElement('span')
+        highlightag(randomTag)
 
+        setTimeout(()=>{
+            unHighlightTag(randomTag)
+        },100)
+    }, 100);
+
+    setTimeout(()=>{
+        clearInterval(interval)
+
+        setTimeout(()=>{
+            const randomTag = pickRandomTag()
+            highlightag(randomTag)
+        }, 100)
+
+    }, times *100)
 }
 
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random()* tags.length)]
+}
 
+function highlightag(tag) {
+    tag.classList.add('highlight')
+}
 
-
-
-
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
