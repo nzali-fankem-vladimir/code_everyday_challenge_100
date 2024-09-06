5419ff18025529fd23b23c65e7eb7f1d7155c9ff
nzali-fankem-vladimir <diminzali@gmail.com>
2024-09-06 13:33:33 +0100

    version tutorielle du day20

--- day 20/script.js
+++ day 20/script.js
@@ -1,22 +1,22 @@
-document.querySelector('.btn').addEventListener('click', function (event) {
-    const button = event.currentTarget;
-    const ripple = document.createElement('span');
-    
-    // Définir les dimensions de l'onde
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-    
-    // Appliquer le style à l'onde
-    ripple.style.width = ripple.style.height = `${diameter}px`;
-    ripple.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-    ripple.style.top = `${event.clientY - button.offsetTop - radius}px`;
-    ripple.classList.add('ripple');
-    
-    // Ajouter l'onde au bouton
-    button.appendChild(ripple);
+const buttons = document.querySelector('.ripple')
 
-    // Retirer l'élément de l'onde après l'animation
-    ripple.addEventListener('animationend', () => {
-        ripple.remove();
-    });
-});
+// buttons.forEach(button =>{
+    buttons.addEventListener('click', function(e){
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside  = x - buttonLeft
+        const yInside = y - buttonTop
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(()=>circle.remove, 500)
+    })
+// })
