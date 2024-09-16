5a780896024cae0305d5d580c0854c6626b22033
nzali-fankem-vladimir <diminzali@gmail.com>
2024-09-16 12:18:32 +0100

    version tutorielle du day25

--- day 25/script.js
+++ day 25/script.js
@@ -1,18 +1,11 @@
-const nav_bar = document.querySelector('nav')
-const covering_hero = document.querySelector('.covering_hero')
-const triggerPoint = document.querySelector('.covering_hero h1')
-let trigger = triggerPoint.getBoundingClientRect().top
+const nav = document.querySelector('.nav')
 
+window.addEventListener('scroll', fixNav)
 
-
-
-window.addEventListener('scroll', ()=>{
-    let nav_bottom = nav_bar.getBoundingClientRect().bottom
-    nav_bottom +=  window.scrollY
-    console.log(nav_bottom)
-    if(nav_bottom >= trigger){
-        nav_bar.classList.add('active')
-    }else if(nav_bottom < trigger){
-        nav_bar.classList.remove('active')
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight +150){
+        nav.classList.add('active')
+    }else{
+        nav.classList.remove('active')
     }
-})
+}
